8096d00437c28e48c25dd044fdf22ddaca721346
Diogo Nogueira <diogoclanogueira@gmail.com>
2025-09-18 11:25:37 +0100

    improve layout, and add progress meter for currently playing track

---
+++ app/utils/spotify.ts
@@ -0,0 +1,168 @@
+import { supabase } from '~/utils/supabase'
+
+export async function getAccessToken() {
+	const { data, error } = await supabase
+		.from('spotify_tokens')
+		.select('refresh_token')
+		.eq('id', 'spotify-global-token')
+		.single()
+
+	if (error || !data) throw new Error('Spotify refresh token not found')
+
+	const refreshToken = data.refresh_token
+
+	const body = new URLSearchParams({
+		grant_type: 'refresh_token',
+		refresh_token: refreshToken,
+		client_id: useRuntimeConfig().spotifyClientId,
+		client_secret: useRuntimeConfig().spotifyClientSecret
+	})
+
+	const resp = await fetch('https://accounts.spotify.com/api/token', {
+		method: 'POST',
+		body,
+		headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+	})
+
+	const json = await resp.json()
+	return json.access_token
+}
+
+export async function getCurrentlyPlaying(accessToken: string) {
+	const resp = await fetch(
+		'https://api.spotify.com/v1/me/player/currently-playing',
+		{
+			headers: { Authorization: `Bearer ${accessToken}` }
+		}
+	)
+
+	if (resp.status !== 200) return null
+	const data: SpotifyResponse = await resp.json()
+	if (!data || !data.item) return null
+
+	return {
+		artist: data.item.artists.map(artist => artist.name).join(', '),
+		track: data.item.name,
+		album: data.item.album.name,
+		albumImageUrl: data.item.album.images[0]?.url,
+		isPlaying: data.is_playing,
+		progressMs: data.progress_ms,
+		durationMs: data.item.duration_ms,
+		trackUrl: data.item.external_urls.spotify
+	}
+}
+
+export type SpotifyResponse = {
+	device: Device
+	repeat_state: string
+	shuffle_state: boolean
+	context: Context
+	timestamp: number
+	progress_ms: number
+	is_playing: boolean
+	item: Item
+	currently_playing_type: string
+	actions: Actions
+}
+
+export type Actions = {
+	interrupting_playback: boolean
+	pausing: boolean
+	resuming: boolean
+	seeking: boolean
+	skipping_next: boolean
+	skipping_prev: boolean
+	toggling_repeat_context: boolean
+	toggling_shuffle: boolean
+	toggling_repeat_track: boolean
+	transferring_playback: boolean
+}
+
+export type Context = {
+	type: string
+	href: string
+	external_urls: ExternalUrls
+	uri: string
+}
+
+export type ExternalUrls = {
+	spotify: string
+}
+
+export type Device = {
+	id: string
+	is_active: boolean
+	is_private_session: boolean
+	is_restricted: boolean
+	name: string
+	type: string
+	volume_percent: number
+	supports_volume: boolean
+}
+
+export type Item = {
+	album: Album
+	artists: Artist[]
+	available_markets: string[]
+	disc_number: number
+	duration_ms: number
+	explicit: boolean
+	external_ids: ExternalIDS
+	external_urls: ExternalUrls
+	href: string
+	id: string
+	is_playable: boolean
+	linked_from: LinkedFrom
+	restrictions: Restrictions
+	name: string
+	popularity: number
+	preview_url: string
+	track_number: number
+	type: string
+	uri: string
+	is_local: boolean
+}
+
+export type Album = {
+	album_type: string
+	total_tracks: number
+	available_markets: string[]
+	external_urls: ExternalUrls
+	href: string
+	id: string
+	images: Image[]
+	name: string
+	release_date: string
+	release_date_precision: string
+	restrictions: Restrictions
+	type: string
+	uri: string
+	artists: Artist[]
+}
+
+export type Artist = {
+	external_urls: ExternalUrls
+	href: string
+	id: string
+	name: string
+	type: string
+	uri: string
+}
+
+export type Image = {
+	url: string
+	height: number
+	width: number
+}
+
+export type Restrictions = {
+	reason: string
+}
+
+export type ExternalIDS = {
+	isrc: string
+	ean: string
+	upc: string
+}
+
+export type LinkedFrom = {}
--- server/api/readme.get.ts
+++ server/api/readme.get.ts
@@ -1,7 +1,8 @@
 import { b64Outfit } from '~/utils/font'
-import { getAccessToken, getCurrentlyPlaying } from '~/utils/supabase'
+import { getAccessToken, getCurrentlyPlaying } from '~/utils/spotify'
 
 const GITHUB_README_WIDTH = '100%' //980 dont change this unless you want to make it smaller.
+const GITHUB_README_HEIGHT = '100%'
 
 const NAME = 'Diogo Nogueira'
 const GH_USERNAME = 'isneru'
@@ -16,158 +17,230 @@ const TECH_STACK = [
 ]
 
 export default defineEventHandler(async event => {
-	let nowPlayingText = 'Nothing is playing right now'
-	try {
-		const accessToken = await getAccessToken()
-		const nowPlaying = await getCurrentlyPlaying(accessToken)
-		if (nowPlaying) {
-			nowPlayingText = `${nowPlaying.artist} - ${nowPlaying.track}`
-		}
-	} catch (err) {
-		console.error('Erro Spotify:', err)
-	}
+	const accessToken = await getAccessToken()
+	const nowPlaying = await getCurrentlyPlaying(accessToken)
+
+	const toSeconds = (ms: number) => Math.floor(ms / 1000)
+
+	let progress = nowPlaying?.isPlaying ? nowPlaying.progressMs : 0
+	let duration = nowPlaying?.isPlaying ? nowPlaying.durationMs : 0
 
 	const svg = `
-	<svg xmlns="http://www.w3.org/2000/svg" width="${GITHUB_README_WIDTH}" height="400" aria-labelledby="title" role="img">
-  	<title id="title">${GH_USERNAME}'s readme</title>
-  	<foreignObject width="${GITHUB_README_WIDTH}" height="400">
-		<style>
-			@font-face {
-				font-family: 'Outfit';
-				src: url('data:font/woff2;base64,${b64Outfit}') format('woff2');
-				font-weight: 100 900;
-				font-style: normal;
-			}
+	<svg xmlns="http://www.w3.org/2000/svg" width="${GITHUB_README_WIDTH}" height="${GITHUB_README_HEIGHT}" aria-labelledby="title" role="img">
+		<title id="title">${GH_USERNAME}'s readme</title>
+		<foreignObject width="100%" height="100%">
+			<style>
+				@font-face {
+					font-family: 'Outfit';
+					src: url('data:font/woff2;base64,${b64Outfit}') format('woff2');
+					font-weight: 100 900;
+					font-style: normal;
+				}
 
-			:root {
-				--color-burnt-sienna-1: #d77a61;
-				--color-burnt-sienna-2: #d8b4a0;
-				--color-burnt-sienna-3: #dbd3d8;
-				--color-antiflash-white: #eff1f3;
-				--color-gunmetal: #223843;
-				--color-gunmetal-muted: #5e6e7e;
-			}
+				:root {
+					--color-burnt-sienna-1: #d77a61;
+					--color-burnt-sienna-2: #d8b4a0;
+					--color-burnt-sienna-3: #dbd3d8;
+					--color-antiflash-white: #eff1f3;
+					--color-gunmetal: #223843;
+					--color-gunmetal-muted: #5e6e7e;
+				}
 
+				* {
+					font-smooth: antialiased;
+					-webkit-font-smoothing: antialiased;
+					-moz-osx-font-smoothing: grayscale;
+					box-sizing: border-box;
+					margin: 0;
+					padding: 0;
+					font-family: 'Outfit', sans-serif;
+					color: var(--color-gunmetal);
+				}
 
-			* {
-				font-smooth: antialiased;
-				-webkit-font-smoothing: antialiased;
-				-moz-osx-font-smoothing: grayscale;
-				box-sizing: border-box;
-				margin: 0;
-				padding: 0;
-				font-family: 'Outfit', sans-serif;
-				color: var(--color-gunmetal);
-			}
+				.container {
+					display: flex;
+					flex-wrap: wrap;
+					width: 100%;
+					background: var(--color-antiflash-white);
+					padding: 2rem;
+					gap: 3rem;
+					border-radius: 30px;
+					box-sizing: border-box;
+				}
 
-			.container {
-				display: flex;
-				width: 100%;
-				background: var(--color-antiflash-white);
-				padding: 2rem;
-				gap: 3rem;
-				border-radius: 30px;
-			}
+				.about {
+					display: flex;
+					flex-direction: column;
+					gap: 1rem;
+					flex: 1 1 300px;
+					min-width: 200px;
+				}
 
-			.about {
-				display: flex;
-				flex-direction: column;
-				gap: 1rem;
-				flex: 1;
-			}
+				.name {
+					font-weight: 800;
+					color: var(--color-gunmetal);
+					font-size: 2rem;
+					word-break: break-word;
+				}
 
-			.name {
-				font-weight: 800;
-				color: var(--color-gunmetal);
-				font-size: 2rem;
-			}
+				.username {
+					font-weight: 500;
+					font-size: 1.2rem;
+					color: var(--color-burnt-sienna-1);  
+					font-style: italic;
+					margin-left: 0.5rem;
+				}
 
-			.username {
-				font-weight: 500;
-				font-size: 1.2rem;
-				color: var(--color-burnt-sienna-1);	
-				font-style: italic;
-				margin-left: 0.5rem;
-			}
+				.description {
+					font-weight: 400;
+					font-size: 1.2rem;
+					word-break: break-word;
+					text-wrap: balance;
+				}
 
-			.description {
-				font-weight: 400;
-				font-size: 1.2rem;
-			}
+				.topic {
+					font-weight: 700;
+					font-size: 1.5rem;
+					margin-left: -1ch;
+				}
 
-			.topic {
-				font-weight: 700;
-				font-size: 1.5rem;
-				margin-left: -1ch;
-			}
+				.topic::before {
+					content: '# ';
+					font-size: 1.2rem;
+					color: var(--color-burnt-sienna-1);
+					top: 4px;
+					left: -48px;
+					width: 32px;
+					height: 32px;
+					font-weight: 700;
+				}
 
-			.topic::before {
-				content: '# ';
-				font-size: 1.2rem;
-				color: var(--color-burnt-sienna-1);
-				top: 4px;
-				left: -48px;
-				width: 32px;
-				height: 32px;
-				font-weight: 700;
-			}
+				hr {
+					border: none;
+					height: 1px;
+					background: var(--color-burnt-sienna-3);
+					margin: 1rem 0;  
+				}
 
-			hr {
-				border: none;
-				height: 1px;
-				background: var(--color-burnt-sienna-3);
-				margin: 1rem 0;	
-			}
+				ul {
+					list-style: none;
+					font-weight: 500;
+					display: flex;
+					flex-direction: column;
+					gap: 0.5rem;
+				}
 
-			ul {
-				list-style: none;
-				font-weight: 500;
-				display: flex;
-				flex-direction: column;
-				gap: 0.5rem;
-			}
+				li span.tech {
+					margin-bottom: 0.5rem;
+					font-size: 1.1rem;
+					text-decoration: var(--color-burnt-sienna-1) wavy underline;
+					font-weight: 600;
+				}
 
-			li span.tech {
-				margin-bottom: 0.5rem;
-				font-size: 1.1rem;
-				text-decoration: var(--color-burnt-sienna-1) wavy underline;
-				font-weight: 600;
-				
-			}
+				li span.tech-list {
+					font-weight: 400;
+				}
 
-			li span.tech-list {
-				font-weight: 400;
-			}
+				.playing {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					gap: 1rem;
+					max-width: 350px;
+					flex: 1 1 250px;
+					min-width: 200px;
+					width: 100%;
+				}
 
-		
+				.track {
+					font-weight: 600;
+					max-width: 90%;
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					text-align: center;
+				}
 
+				.album {
+					display: block;
+					border-radius: 12px;
+					width: 100%;
+					max-width: 200px;
+					aspect-ratio: 1 / 1;
+				}
 
+				.meter { 
+					height: 5px;
+					position: relative;
+					background-color: var(--color-gunmetal);
+					overflow: hidden;
+					border-radius: 999px;
+					width: 100%;
+					max-width: 200px;
+					margin: 0 auto;
+				}
 
-		</style>
-		<div class="container" xmlns="http://www.w3.org/1999/xhtml">
-			<div class="about">
-				<p class="name">${NAME} <span class="username">${GH_USERNAME}</span></p>
+				.meter span {
+					display: block;
+					height: 100%;
+				}
 
-				<p class="description">${DESCRIPTION}</p>
+				.progress {
+					background-color: var(--color-burnt-sienna-1);
+					animation: progressBar ${toSeconds(duration - progress)}s ease-in-out;
+					animation-fill-mode:both;
+				}
 
-				<hr />
+				@keyframes progressBar {
+					0% { width: ${nowPlaying?.isPlaying ? `${(progress / duration) * 100}%` : '0%'}; }
+					100% { width: 100%; }
+				}
 
-				<p class="topic">Tech Stack</p>
+				@media (max-width: 760px) {
+					.container {
+						flex-direction: column;
+					}
+					.playing {
+						align-items: flex-start;
+						min-width: 0;
+						max-width: 250px;
+					}
+					.about {
+						min-width: 0;
+					}
+				}
 
-				<ul>
-					<li><span class="tech">${TECH_STACK[0].name}:</span><span class="tech-list"> ${TECH_STACK[0].list}</span></li>
-					<li><span class="tech">${TECH_STACK[1].name}:</span><span class="tech-list"> ${TECH_STACK[1].list}</span></li>
-					<li><span class="tech">${TECH_STACK[2].name}:</span><span class="tech-list"> ${TECH_STACK[2].list}</span></li>
-					<li><span class="tech">${TECH_STACK[3].name}:</span><span class="tech-list"> ${TECH_STACK[3].list}</span></li>
-				</ul>
-			</div>
-			<div class="playing">
-				<p class="topic">Currently Playing</p>
-				<p>${nowPlayingText}</p>
-			</div>
-		</div>
-	</foreignObject>
-</svg>`
+			
+				}
+			</style>
+			<div class="container" xmlns="http://www.w3.org/1999/xhtml">
+				<div class="about">
+					<p class="name">${NAME} <span class="username">${GH_USERNAME}</span></p>
+					<p class="description">${DESCRIPTION}</p>
+					<hr />
+					<p class="topic">Tech Stack</p>
+					<ul>
+						<li><span class="tech">${TECH_STACK[0].name}:</span><span class="tech-list"> ${TECH_STACK[0].list}</span></li>
+						<li><span class="tech">${TECH_STACK[1].name}:</span><span class="tech-list"> ${TECH_STACK[1].list}</span></li>
+						<li><span class="tech">${TECH_STACK[2].name}:</span><span class="tech-list"> ${TECH_STACK[2].list}</span></li>
+						<li><span class="tech">${TECH_STACK[3].name}:</span><span class="tech-list"> ${TECH_STACK[3].list}</span></li>
+					</ul>
+				</div>
+				<div class="playing">
+					<p class="topic">Currently Playing</p>
+						${
+							nowPlaying?.track
+								? `<img class="album" src="${nowPlaying.albumImageUrl}" alt="${nowPlaying.track}" />
+										<p title="${nowPlaying.artist} - ${nowPlaying.track}" class="track">${nowPlaying.artist} - ${nowPlaying.track}</p>
+										<div class="meter">
+											<span class="progress" style="width: ${nowPlaying.isPlaying ? `${(progress / duration) * 100}%` : '0%'}"></span>
+										</div>`
+								: `<p class="track">Nothing is playing right now</p>`
+						}
+						</div>
+					</div>
+		</foreignObject>
+	</svg>`
 
 	setHeader(event, 'Content-Type', 'image/svg+xml')
 	setHeader(event, 'Cache-Control', 's-maxage=1, stale-while-revalidate')
